b7f88c4c220e9d708166db9dbc28b993cb7c4dba
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-09-03 18:12:55 -0500

    Adding projects

---
+++ Basic_C#_Programs/myConsoleProject.cs/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class CarInsurance
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Car Insurance Application");
+        Console.WriteLine(" What is your age ?");
+        int age = Convert.ToInt32(Console.ReadLine());
+    }
+}
---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class CarInsurance
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Car Insurance Application");
+        Console.WriteLine(" What is your age ?");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Have you ever had a DUI? , Yes or No ");
+        string answer = Console.ReadLine().ToLower();
+        bool dui = answer == "yes";
+        Console.WriteLine(" How many speeding tickets do you have? ");
+        int tickets = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Qualified for insurance? ");
+        bool qualified= ( age > 15) && !dui && ( tickets < 3);
+        Console.WriteLine(qualified);
+
+    }
+}
+
+
+/*
+ Use the following qualification rules to determine if the applicant qualifies for car insurance:
+
+Applicants must be over 15 years old.
+
+Applicants must not have any DUI’s.
+
+Applicants must not have more than 3 speeding tickets.
+
+
+ 
+ */
