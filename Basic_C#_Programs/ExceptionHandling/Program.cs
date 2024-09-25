3bf0a6a9b2fe619e8f0d425121a66f8223d9fc72
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-09-25 17:06:17 -0500

    ExceptionHandling
    Added examples

---
+++ Basic_C#_Programs/ExceptionHandling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            //  Exceptions Handling gives you the ability to put in Custom error messages, log information to a database, log errors. 
+            try
+            {
+
+                Console.WriteLine("Pick a  number : ");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number :");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing number " + numberOne + " by " + numberTwo);
+                int numberDivided = numberOne / numberTwo;
+                Console.WriteLine($" the result of dividing {numberOne} by {numberTwo} = {numberDivided}");
+                Console.ReadLine();
+
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine(" Please type a whole number ..");
+
+            }
+
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine(" Don't divide by zero ....");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+            finally {
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringsAndIntegersAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
