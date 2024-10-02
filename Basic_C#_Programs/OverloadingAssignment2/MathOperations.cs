812a54bf9e224cf5d026d3b73378204ddd76d282
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-10-02 15:34:22 -0500

    overloading
    Added assignment#2

---
+++ Basic_C#_Programs/OverloadingAssignment2/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. 
+Have the method do a math operation and return an integer result.
+ 
+ */
+namespace OverloadingAssignment2
+{
+    public class MathOperations
+    {
+        public int performOperation(int num1, int num2 = 10) { 
+        
+           int result = num1 + num2;
+            return result;
+        
+        }
+    }
+}
---
+++ Basic_C#_Programs/OverloadingAssignment2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OverloadingAssignment2
+{
+
+    /*
+     In the Main() method of the console app, instantiate the class.
+
+    Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+
+    Call the method in the class, passing in the one or two numbers entered.
+
+Try various combinations of numbers on the code, including having no second number.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MathOperations mathOperations = new MathOperations();
+
+            Console.WriteLine("please enter the first number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number, or just press enter...");
+
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int result = mathOperations.performOperation(num1, num2);
+                Console.WriteLine($" the sum of {num1} & {num2} is {result}");
+            }
+            catch
+            {
+                int result = mathOperations.performOperation(num1);
+                Console.WriteLine($"{num1} + Default 10 = {result}");
+                
+            
+            }
+        }
+    }
+}
