7b3da748d3970d2d168944543d8cebf6b87babaa
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-10-03 16:16:21 -0500

    Assignment Submission
    adding the assignment to be reviewed

---
+++ Basic_C#_Programs/MethodClassAssignmentSubmission/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignmentSubmission
+{
+
+    /*
+     
+    Create a class. In that class, create a void method that takes two integers as parameters. 
+    Have the method do a math operation on the first integer and display the second integer to the screen. 
+     
+     */
+
+    // Clas that contains a method that performs a math operation.
+    class Operations
+    {
+        //void method that takes 2 integeres as parameters.
+        public void MathOp(int num1, int num2)
+        {
+            //Perform a math operation on the first integer
+            int result = num1 * 5;
+
+            //Display the result of the operation
+            Console.WriteLine($"The result of the operation on the first number is {result} where {num1} is Multiplied by 5 ");
+
+            //Display the second integer as it was passed.
+            Console.WriteLine($"The second number is {num2}");
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignmentSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodClassAssignmentSubmission
+{
+
+    /*
+     
+    In the Main() method of the console app, instantiate the class.
+
+    Call the method in the class, passing in two numbers. 
+
+    Call the method in the class, specifying the parameters by name.
+
+    
+     
+     */
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperation class.
+           Operations operations = new Operations();
+            //Call the method with two numbers, passing them directly.
+            Console.WriteLine("Calling method with direct arguments: ");
+            operations.MathOp(10,5);
+            // Call the method with named parameters.
+            Console.WriteLine("Calling methos with named parameters:");
+            operations.MathOp(num1:20, num2: 5);
+            // Keep the console window open.
+            Console.ReadLine();
+        }
+    }
+}
