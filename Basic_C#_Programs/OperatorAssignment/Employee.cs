028dc3e904148f21381fddbce66a5466a7624c10
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-10-07 15:00:34 -0500

    Adding Assignment
    Operator overloading assignment

---
+++ Basic_C#_Programs/OperatorAssignment/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorAssignment
+{
+
+    // Employee class with properties Id, FirstName, and LastName
+    public class Employee
+    {
+        // Auto-implemented properties for Id, FirstName, and LastName
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload the == operator to compare two Employee objects based on their Id property
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // If both are null, they are considered equal
+            if (ReferenceEquals(emp1, null) && ReferenceEquals(emp2, null))
+                return true;
+
+            // If one is null and the other is not, they are considered different
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+                return false;
+
+            // Compare their Id properties
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload the != operator to complement the == operator
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            // Reuse the logic of the == operator, returning the opposite
+            return !(emp1 == emp2);
+        }
+
+       
+    }
+}
---
+++ Basic_C#_Programs/OperatorAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OperatorAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Employee objects with different Ids and names
+            Employee emp1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
+            Employee emp3 = new Employee { Id = 1, FirstName = "Mike", LastName = "Brown" };
+
+            // Compare emp1 and emp2 using the overloaded == operator
+            if (emp1 == emp2)
+            {
+                // Since their Ids are different, this block will not be executed
+                Console.WriteLine("Employee 1 and Employee 2 are the same.");
+            }
+            else
+            {
+                // This will be printed because emp1 and emp2 have different Ids
+                Console.WriteLine("Employee 1 and Employee 2 are different.");
+            }
+
+            // Compare emp1 and emp3 using the overloaded == operator
+            if (emp1 == emp3)
+            {
+                // Since emp1 and emp3 have the same Id, this block will be executed
+                Console.WriteLine("Employee 1 and Employee 3 are the same.");
+            }
+            else
+            {
+                // This block will not be executed because their Ids are the same
+                Console.WriteLine("Employee 1 and Employee 3 are different.");
+            }
+        }
+    }
+}
